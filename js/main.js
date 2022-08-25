a537811e80096ac1e7702e0fee7e78d08c2acf62
Guilherme <ggualbertosouza@gmail.com>
2022-08-25 18:26:38 -0300

    estilizando a pagina

--- js/main.js
+++ js/main.js
@@ -5,24 +5,16 @@ btn1.addEventListener('click', loadpk())
 
 
 function loadpk(){
-    try{
+    
         (async function(){
             const response = await fetch('https://pokeapi.co/api/v2/pokemon/25');
             const jsonData = await response.json();
-            
-            for (const pokemon of jsonData){
-               console.log(pokemon.results)
             }
-     
-            var name = document.getElementById('name');
-            name.innerHTML = pk
-        })();
+
+        ) ();
     } catch (e) {
         console.log('Error:') 
-    }
-     
-    
-        
-}
+    }       
+
 
     
